3fac4ccf65b5ff72bfa9ee0ce506fb02c8ecb1cf
Ruthvik <ramisettyruthvik123@gmail.com>
2023-10-31 21:39:16 -0400

    Create Assignment_2.cpp
    added second assignment

---
+++ Assignment_2.cpp
@@ -0,0 +1,86 @@
+#include <iostream>
+
+struct SparseMat
+{
+    int t_rows, t_cols;
+    int **array;
+};
+
+SparseMat createSparseMatrix(int rows, int cols)
+{
+    SparseMat matrix;
+    matrix.t_rows = rows;
+    matrix.t_cols = cols;
+
+    matrix.array = new int *[rows];
+    int i = 0;
+    while (i < rows)
+    {
+        matrix.array[i] = new int[3]();
+        i++;
+    }
+    return matrix;
+}
+
+void addNum(SparseMat &matrix, int row, int col, int number)
+{
+    for (int i = 0; i < matrix.t_rows; i++)
+    {
+        if (matrix.array[i][0] == -1)
+        {
+            matrix.array[i][0] = row;
+            matrix.array[i][1] = col;
+            matrix.array[i][2] = number;
+            break;
+        }
+    }
+}
+
+int main()
+{
+    int rows, cols, nonZero;
+    std::cout << "enter number of rows: ";
+    std::cin >> rows;
+    std::cout << "enter number of columns: ";
+    std::cin >> cols;
+    std::cout << "enter number of Non zero elements: ";
+    std::cin >> nonZero;
+
+    SparseMat matrix = createSparseMatrix(nonZero, 3);
+
+    for (int i = 0; i < matrix.t_rows; i++)
+    {
+        matrix.array[i][0] = -1;
+    }
+
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < cols; j++)
+        {
+            int value;
+            std::cout << "Enter value for element at row " << i << " and column " << j << ": ";
+            std::cin >> value;
+            if (value != 0)
+            {
+                addNum(matrix, i, j, value);
+            }
+        }
+    }
+
+    std::cout << "Sparse Matrix:" << std::endl;
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < cols; j++)
+        {
+            for (int k = 0; k < matrix.t_rows; k++)
+            {
+                if (matrix.array[k][0] == i && matrix.array[i][1] == j)
+                {
+                    std::cout << matrix.array[k][2] << " ";
+                }
+            }
+        }
+        std::cout << std::endl;
+    }
+    return 0;
+}
