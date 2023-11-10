86d35101cc4af9f1f14dbf5f257fe7adde218c4d
ramisettyruthvik123 <149131877+ramisettyruthvik123@users.noreply.github.com>
2023-11-09 22:12:07 -0500

    Update Assignment_3.cpp
    added comments

--- Assignment_3.cpp
+++ Assignment_3.cpp
@@ -1,65 +1,77 @@
 #include <iostream>
 #include <stdexcept>
 
-class Node {
+//class Node represents sparse matrix element
+class ElementNode {
 public:
     int value;
-    int row;
-    int col;
-    Node* next;
+    int total_rows;
+    int total_cols;
+    ElementNode* next;
 
-    Node(int val, int r, int c) : value(val), row(r), col(c), next(nullptr) {}
+    ElementNode(int val, int r, int c) {
+        value = val;
+        total_rows = r;
+        total_cols = c;
+        next = nullptr;
+    } 
 };
 
-class SparseMatrix {
-private:
+class Sparse {
+public:
     int rows;
     int cols;
-    Node** heads;
+    ElementNode** heads;
 
-public:
-    SparseMatrix(int r, int c) : rows(r), cols(c) {
-        heads = new Node*[rows]();
+    Sparse(int r, int c) {
+        rows = r;
+        cols =  c;
+        heads = new ElementNode*[rows]();
     }
 
-    void insert(int value, int row, int col) {
-        if (row < 0 || row >= rows || col < 0 || col >= cols) {
-            throw std::out_of_range("Row or column index out of bounds");
+    void insertElement( int row, int col,int value) {
+        if (row < 0  || col >= cols || row >= rows || col < 0) {
+            throw std::out_of_range("Index out of bounds");
         }
 
-        Node* new_node = new Node(value, row, col);
+        ElementNode* new_node = new ElementNode(value, row, col);
         if (!heads[row]) {
             heads[row] = new_node;
         } else {
-            Node* current = heads[row];
+            ElementNode* current = heads[row];
             while (current->next) {
                 current = current->next;
             }
             current->next = new_node;
         }
     }
+};
 
-    int get(int row, int col) {
-        if (row < 0 || row >= rows || col < 0 || col >= cols) {
-            throw std::out_of_range("Row or column index out of bounds");
-        }
+int main() {
+    
+    std::cout << "Enter the number of rows: ";
+    int cols, rows;
+    std::cin >> rows;
+    std::cout << "Enter the number of columns: ";
+    std::cin >> cols;
 
-        Node* current = heads[row];
-        while (current) {
-            if (current->col == col) {
-                return current->value;
-            }
-            current = current->next;
+    Sparse matrix(rows, cols);
+
+    for (int i = 0; i < rows; i++) {
+        for (int j = 0; j < cols; j++) {
+            int value;
+            std::cout << "Enter value at row " << i << " and column " << j << ": ";
+            std::cin >> value;
+            matrix.insertElement(i, j,value);
         }
-        return 0;  // Return 0 for elements not explicitly set
     }
 
-    void display() {
-        for (int row = 0; row < rows; ++row) {
-            Node* current = heads[row];
+    std::cout << "**********Sparse Matrix:**********\n";
+            for (int row = 0; row < rows; ++row) {
+            ElementNode* current = matrix.heads[row];
             int row_values[cols] = {0};
             while (current) {
-                row_values[current->col] = current->value;
+                row_values[current->total_cols] = current->value;
                 current = current->next;
             }
             for (int col = 0; col < cols; ++col) {
@@ -67,35 +79,6 @@ public:
             }
             std::cout << std::endl;
         }
-    }
-};
-
-int main() {
-    int rows, cols;
-    std::cout << "Enter the number of rows: ";
-    std::cin >> rows;
-    std::cout << "Enter the number of columns: ";
-    std::cin >> cols;
-
-    SparseMatrix matrix(rows, cols);
-
-    for (int i = 0; i < rows; i++) {
-        for (int j = 0; j < cols; j++) {
-            int value;
-            std::cout << "Enter value at row " << i << " and column " << j << ": ";
-            std::cin >> value;
-            matrix.insert(value, i, j);
-        }
-    }
-
-    std::cout << "Sparse Matrix:\n";
-    matrix.display();
-
-    int search_row, search_col;
-    std::cout << "Enter the row and column to retrieve a value: ";
-    std::cin >> search_row >> search_col;
-    int result = matrix.get(search_row, search_col);
-    std::cout << "Value at (" << search_row << ", " << search_col << ") is " << result << std::endl;
 
     return 0;
 }
